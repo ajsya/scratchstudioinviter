4c4cecf3c805b93859f6e1ed055ad6c193b68184
ajsya <52182942+ajsya@users.noreply.github.com>
2021-10-29 23:32:32 -0400

    Rewritten for scratchconnect
    Rewritten for scratchconnect due to scratchclient failing to work because of a update to the scratch api

--- main.py
+++ main.py
@@ -1,39 +1,35 @@
-from scratchclient import ScratchSession
-
-import requests
+import scratchconnect
 import json
+import requests
 import time
 
-session = ScratchSession("", "") #username and password
-
-studio = 30290420 #studio you want to watch
-r = requests.get('https://api.scratch.mit.edu/studios/{0}/comments/?limit=40&offset=0'.format(studio))
-
-print(r.content)
-obj = json.loads(r.content)
-message = obj[0]['content']
-print(message)
+user = scratchconnect.ScratchConnect("346383", "iamabot")
+studio = user.connect_studio(studio_id=30290420)
+scratch_studio = 30290420
 
 last_message = None
 while True:
-    r = requests.get('https://api.scratch.mit.edu/studios/{0}/comments/?limit=40&offset=0'.format(studio))
-    obj = json.loads(r.content)
-    message = obj[0]['content']
-    author = obj[0]['author']['username']
+    r = requests.get('https://api.scratch.mit.edu/studios/{0}/comments/?limit=40&offset=0'.format(scratch_studio))
+    #print(r.content)
+    obj = json.loads(r.text)
+    #print (json.dumps(obj, indent=2))
+
+    comment_content = obj[0]["content"]
+    comment_author = obj[0]["author"]["username"]
     
     keywords = ["invite me", "Invite Me", "invite Me", "Invite me", "iNvItE mE", "InViTe Me", "INVITE ME"]
 
-    wants_invited = any(keywords in message for keywords in keywords)
+    wants_invited = any(keywords in comment_content for keywords in keywords)
     
     if wants_invited == True:
-        if message != last_message:
-            print(author)
-            session.get_studio(studio).invite_curator(author)
-            last_message = message
-            print("invited")
+        if comment_content != last_message:
+            print(comment_author, " wants invited.")
+            studio.invite_curator(comment_author)
+            last_message = comment_content
+            print(comment_author, " successfully invited! :)")
         else:
-            print("already invited")
+            print("User has been already invited.")
     else:
-        print("does not want invited")
+        print("Author of latest comment does not want to be invited.")
 
-    time.sleep(5)
+    time.sleep(5) #Increase distance between requests, for testing purposes set at every 5 seconds.
